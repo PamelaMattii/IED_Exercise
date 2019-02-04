fecd90ba1cc3b15dfe7c6032591890a0be3bb297
ieduser <ieduser@fi-lab10-21.ied.edu>
2019-02-04 12:21:22 +0100

    added operation

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var myNumber = prompt('Write a number:');
 myNumber = parseInt(myNumber);
 
+if( myNumber < 0){
+    alert('Given number is negative.')
+    myNumber = Math.abs(myNumber);
+}else{
+    alert('Given nuber is positive!')
+}
+
+
 var squareRoot = Math.sqrt(myNumber);
 
 alert(squareRoot);
