d2b4cd2b834a94ae0885e375353127cbf1aedde9
Pamela <pmattii01@gmail.com>
2019-02-11 11:25:41 +0100

    added lesson 3

---
+++ lesson 3/script.js
@@ -0,0 +1,13 @@
+//IF: eseguire delle istruzioni al verificarsi di una condizione
+
+
+const COMPARABLE_NUMBER = 10;
+//CONST: una costante. si scrive in uppercase separata da underscore. non è modificabile una volta dichiarata.
+
+var readedNum = prompt("Scrivi un numero:");
+
+if(readedNum == COMPARABLE_NUMBER){
+    alert("Paragone ok!")
+
+}
+
