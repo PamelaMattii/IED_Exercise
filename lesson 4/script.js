00673e9ed319be369b6f1a1f062afa00f4b55361
Pamela <pmattii01@gmail.com>
2019-02-18 11:40:48 +0100

    added iteration

--- lesson 4/script.js
+++ lesson 4/script.js
@@ -1,3 +1,5 @@
+            //CICLO FOR
+
 const MILK_FOR_A_PORTION_ML = 60;
 
 function printMilkByPieEaters(peopleNumber){
@@ -5,6 +7,32 @@ function printMilkByPieEaters(peopleNumber){
     console.log('Il latte necessario per '+peopleNumber+ ' corrisponde a '+milkTotal+' ml '); //stampiamo in console 
 }
 
+
+
+function printMilkQuantities(){
+
+
 for(var peopleNumber = 1; peopleNumber <= 10; peopleNumber++){
     printMilkByPieEaters(peopleNumber);
+}
+
+           
+}
+printMilkQuantities();
+
+
+//FOREACH: scorrere una lista di elementi e per ognuno averne l'indice
+//da 0 a n-1
+
+
+var names = ['Carl', 'Bill', 'Steve', 'Mark'];
+
+
+function printTarget(name){
+    console.log('Target of today is customer: '+name);
+}
+
+for(var nameIndex in names){
+    var name = names[nameIndex];
+    printTarget(name);
 }
